0308a713c7f85cee92f7e304fdd9403b335221da
Michael Myers <moek@devmoek.com>
2023-07-18 19:42:18 +0400

    feed component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Sidebar from "./components/Sidebar"
+import Feed from "./components/Feed"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto">
       <Sidebar />
+      <Feed />
     </main>  
   )
 }
