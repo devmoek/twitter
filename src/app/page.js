47f4b726820a68d3f39ceafa786b01d3fe1158e7
Michael Myers <moek@devmoek.com>
2023-11-21 23:02:58 +0400

    add widget component and a search bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar"
 import Feed from "./components/Feed"
+import Widgets from "./components/Widgets"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto">
       <Sidebar />
       <Feed />
+      <Widgets />
     </main>  
   )
 }
